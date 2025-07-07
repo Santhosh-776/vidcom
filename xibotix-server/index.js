fbc320773f80354f338ae5900c133bc9a4bc1b79
Santhosh Natarajan <162946821+Santhosh-776@users.noreply.github.com>
2025-07-07 14:34:19 +0530

    Update index.js

--- xibotix-server/index.js
+++ xibotix-server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://vidcom-77.vercel.app/",
+    "https://vidcom-77.vercel.app",
 ];
 
 const serviceAccount = {
