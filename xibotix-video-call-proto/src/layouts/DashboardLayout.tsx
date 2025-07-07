b3232d0101ba5dad87d8be767c31b8932d457b25
Santhosh Natarajan <162946821+Santhosh-776@users.noreply.github.com>
2025-07-07 15:18:24 +0530

    Update DashboardLayout.tsx

--- xibotix-video-call-proto/src/layouts/DashboardLayout.tsx
+++ xibotix-video-call-proto/src/layouts/DashboardLayout.tsx
@@ -67,7 +67,7 @@ export default function DashboardLayout() {
             <footer className="bg-white border-t border-gray-200 py-6">
                 <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-500">
                     <p>
-                        © {new Date().getFullYear()} XiboTix. All rights
+                        © {new Date().getFullYear()}All rights
                         reserved.
                     </p>
                 </div>
